be775c3b022634800da7157381d2ac5e705dc44b
Jon Wenger <jonwenger783@gmail.com>
2018-05-14 21:58:13 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+
+turn_count
+
+def turn_count(board)
+counter = 0
+board.each do |turn|
+  puts "#{{turn}}"
+end
+end
+
+def current_player
+  turn_count
+  if counter%2==0
+    return "x"
+  end
+  return "o"
+
+  
+
+end
+
