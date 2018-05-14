2ca330697a095b7c707bf755d531b21b69ee3584
Jon Wenger <jonwenger783@gmail.com>
2018-05-14 22:16:27 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+##board = ['x','o','  ', '  ', "o", "X", "O"]
+
+
+def turn_count(board)
+counter = 0
+board.each do |turn|
+  ##puts "#{turn}"
+  if turn == "x" || turn == "o" || turn == "X" || turn == "O"
+    counter +=1
+  ##  puts "#{counter}"
+  end
+  end
+  return counter
+end
+
+def current_player(board)
+turn = turn_count(board)
+if turn % 2 == 0
+  return "X"
+end
+return "O"
+
+end
